bf29cf9363aea7b1cd019ce094497d1e95fd736e
codebyteja <chindambhanuteja060@gmail.com>
2025-05-15 13:32:40 +0530

    Add files via upload

---
+++ 02_js variable declaration/basic script.js
@@ -0,0 +1,10 @@
+/*let is a keyword to declare avariable
+a=> variable name/reference variable
+=-> assignmanet operator
+10->value
+*/
+let a =10
+let b=20
+let sum=a+b
+console.log("the sum of"   +a+   '&'  +b+ 'is',sum)
+console.log(`the sum of ${a} , ${b} is ${sum}`)
