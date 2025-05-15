9f77a92eac2cc5ae9c770b392bc0bfe7fc48339f
codebyteja <chindambhanuteja060@gmail.com>
2025-05-15 12:47:29 +0530

    Add files via upload

---
+++ javascript 1 full course/javascript basic/basic script.js
@@ -0,0 +1,26 @@
+//BASIC OF JAVA SCRIPT
+/*=========BASIC MESSAGES OF JS=======
+1)LOG MESSAGES
+2)INFO MESSAGE
+3)TABLE MESSAGE
+4)WARNING MESSAGES
+5)ERROR MESSAGES
+6)TABLE MESSAGE
+THESE ARE THE BASIC JS BASIC MESSAGES*/
+console.log("========log messages========")
+console.log("i am a log message")
+console.log("======info message======")
+console.log("i am a info message")
+console.log("========table mess========")
+let student={
+    name:"teja",
+    age:20,
+    course:'ece'
+}
+console.table(student)
+console.log("=====i am a error message=====")
+console.error("i am a error message")
+console.log("=====i am a warn message=====")
+console.warn("i ama warn message")
+
+
